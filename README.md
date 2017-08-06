b48acdedc26644af45ee4b3fdbe748c12b458ee3
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-08-06 14:56:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Babel-Generate-Guard-Clause
+
+A helper to generate different guard clauses.
+
+## Exports
+
+### `generateTypeCheck(typeAssertion, options)({ name: argumentName })`
+
+#### `generateTypeCheck("number")({ name: argumentName })`
+
+```js
+const typeCheckAst = genertateTypeCheck("string")({ name: "ponies" });
+```
+
+Generated Code:
+```js
+if (typeof ponies !== "string") {
+    throw new Error('ponies should be a string, but got ' + ponies + '(' + typeof ponies +  ')');
+}
+```
+
+#### `generateTypeCheck("number", { selector: "selector" })({ name: argumentName })`
+
+```js
+const typeCheckAst = generateTypeCheck("number", { selector: "x" })({ name: "point" });
+```
+
+Generated Code:
+```js
+if (typeof point.x !== "number") {
+    throw new Error('point.x should be a number, but got ' + point.x + '(' + typeof point.x +  ')');
+}
+```
+
+### `generateIsOneOfCheck`
+
+#### `generateIsOneOfCheck(optionArray)({ name: "argName" })`
+
+```js
+const typeCheckAst = generateIsOneOfCheck(["option1", "option2"])({ name: "argName" });
+```
+
+Generated Code:
+```js
+if (!['option1', 'option2'].some(x => x === argName)) {
+   return new Error("argName should be one of ['option1', 'option2'], but got " + argName);
+}
+```
---
+++ index.js
@@ -0,0 +1,105 @@
+const t = require("babel-types");
+
+// Helper methods to easily stick together type checks
+// generates the following JS:
+// if (CHECK_GENERATOR) {
+//   throw new Error(ERROR_STRING_GENERATOR);
+// }
+const generateCheck = (checkGenerator, errorStringGenerator) => ({
+  type,
+  name
+}) =>
+  t.ifStatement(
+    checkGenerator({ type, name }),
+    t.throwStatement(
+      t.newExpression(t.identifier("Error"), [errorStringGenerator({ name })])
+    )
+  );
+
+// generates "typeof argName !== 'number'"
+const typeCheckTestGenerator = (typeAssertion, options) => ({ name }) =>
+  t.binaryExpression(
+    "!==",
+    t.unaryExpression(
+      "typeof",
+      options.selector
+        ? t.memberExpression(t.identifier(name), t.identifier(options.selector))
+        : t.identifier(name)
+    ),
+    t.stringLiteral(typeAssertion)
+  );
+
+// helper for generating verbose errors, generates
+// throw new Error('argName should be a number, but got ' + argName + '(' + typeof argName +  ')');
+const typeCheckErrorGenerator = (typeAssertion, options) => ({ name }) => {
+  const nameString = options.selector ? `${name}.${options.selector}` : name;
+  const nameAst = options.selector
+    ? t.memberExpression(t.identifier(name), t.identifier(options.selector))
+    : t.identifier(name);
+
+  return t.binaryExpression(
+    "+",
+    t.stringLiteral(
+      nameString + " should be a " + typeAssertion + ", but got "
+    ),
+    t.binaryExpression(
+      "+",
+      nameAst,
+      t.binaryExpression(
+        "+",
+        t.stringLiteral(" ("),
+        t.binaryExpression(
+          "+",
+          t.unaryExpression("typeof", nameAst),
+          t.stringLiteral(")")
+        )
+      )
+    )
+  );
+};
+
+const oneOfCheckTestGenerator = options => ({ name }) =>
+  t.unaryExpression(
+    "!",
+    t.callExpression(
+      t.memberExpression(
+        t.arrayExpression(options.map(option => t.stringLiteral(option))),
+        t.identifier("some")
+      ),
+      [
+        t.arrowFunctionExpression(
+          [t.identifier("option")],
+          t.binaryExpression("===", t.identifier("option"), t.identifier(name))
+        )
+      ]
+    )
+  );
+
+const oneOfCheckErrorGenerator = options => ({ name }) =>
+  t.binaryExpression(
+    "+",
+    t.stringLiteral(
+      name + " should be one of [" + options.join(", ") + "], but got "
+    ),
+    t.identifier(name)
+  );
+
+const generateTypeCheck = (typeAssertion, options = {}) =>
+  generateCheck(
+    typeCheckTestGenerator(typeAssertion, options),
+    typeCheckErrorGenerator(typeAssertion, options)
+  );
+
+// if (!['option1', 'option2'].some(x => x === argName)) {
+//   return new Error("argName should be one of ['option1', 'option2'], but got " + argName);
+// }
+const generateIsOneOfCheck = options =>
+  generateCheck(
+    oneOfCheckTestGenerator(options),
+    oneOfCheckErrorGenerator(options)
+  );
+
+module.exports = {
+  generateTypeCheck,
+  generateIsOneOfCheck
+};
